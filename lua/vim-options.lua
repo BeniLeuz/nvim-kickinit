3760f46f349b7fc4c993c8c1f1e355f6711703c1
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2023-12-29 17:41:14 +0100

    autocomplete works now, nice

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.wo.number = true
 vim.wo.relativenumber = true
+vim.cmd("colorscheme default")
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
