85b870cf2ae43e7635d78d911a2f9a06730c6d1f
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2024-01-02 00:43:09 +0100

    adding css and capabilities properly

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,33 +1,50 @@
 return {
-  {
-    "williamboman/mason.nvim", 
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim", 
-    config = function() 
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rubocop", "ruby_ls", "tsserver"}
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig", 
-    config = function() 
-      local lspconfig = require("lspconfig")
 
-      -- call setup for each lsp that i need
-      -- TODO: automate this?
-      lspconfig.lua_ls.setup({})
-      lspconfig.ruby_ls.setup({})
-      lspconfig.rubocop.setup({})
-      lspconfig.tsserver.setup({})
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup()
+    end,
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = { 'lua_ls', 'rubocop', 'ruby_ls', 'tsserver', 'cssls' },
+      }
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local lspconfig = require 'lspconfig'
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+      }
+
+      lspconfig.ruby_ls.setup {
+        capabilities = capabilities,
+      }
+
+      lspconfig.rubocop.setup {
+        capabilities = capabilities,
+      }
+
+      lspconfig.tsserver.setup {
+        capabilities = capabilities,
+      }
+
+      lspconfig.cssls.setup {
+        capabilities = capabilities,
+      }
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+    end,
+  },
 }
