01c613afd84d6fbce214b2d82c0e6e173b7c947d
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2024-01-17 11:37:15 +0100

    nice closing harpoon like rest of my stuff now

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -15,6 +15,7 @@ return {
 
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<C-c>", function() harpoon.ui:close_menu() end)
 
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
